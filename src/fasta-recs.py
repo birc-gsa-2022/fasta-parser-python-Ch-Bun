4ea7d3dadfc5073305909a20a1e2d18979f61501
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-06 14:14:11 +0200

    Solves fasta-recs.py

--- src/fasta-recs.py
+++ src/fasta-recs.py
@@ -1,4 +1,6 @@
 import argparse
+from multiprocessing import dummy
+from pickle import FALSE
 
 
 def main():
@@ -12,7 +14,20 @@ def main():
     args = argparser.parse_args()
 
     print(f"Now I need to process the records in {args.fasta}")
+    Printer = ''
+    Seq_to_Print = False
+    for line in args.fasta:
 
-
+            if line.startswith('>'):
+                if Seq_to_Print == True:
+                    print(Printer)
+                    Printer = ''
+                    Seq_to_Print = False
+                Printer = line[1:len(line)].strip()
+                Printer = "".join((Printer, " "))
+                Seq_to_Print = True
+            else:
+                Printer = "".join((Printer, line))
+    print(Printer)
 if __name__ == '__main__':
     main()
